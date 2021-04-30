6cd9f0cf47de1e5fdc7c6895f0d0553349dc82f2
mydogspies <mydogspies@zoho.com>
2021-04-30 16:39:45 +0200

    Added config for bringbackdada-front

--- config/remote.config.js
+++ config/remote.config.js
@@ -7,6 +7,14 @@ const config = [
             EXEC: "git pull && npm install && pm2 restart ecosystem.config.js"
         }
     },
+    {
+        REPO: "bringbackdada-front",
+        SETTINGS: {
+            BRANCH: "master",
+            PATH: "/var/www/bringbackdada.com/bringbackdada-front",
+            EXEC: "npm run build && pm2 restart ecosystem.config.js"
+        }
+    }
 ];
 
 module.exports = config;
