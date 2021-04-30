ed9b26316d1ea9943d361389d88cf4da7e7b3621
mydogspies <mydogspies@zoho.com>
2021-04-30 14:09:57 +0200

    Added github PUSH event and config remote site logic

---
+++ config/remote.config.js
@@ -0,0 +1,12 @@
+const config = [
+    {
+        REPO: "bringbackdada-back",
+        SETTINGS: {
+            BRANCH: "master",
+            PATH: "/var/www/bringbackdada.com/bringbackdada-back",
+            EXEC: "git pull && npm install && pm2 restart ecosystem.config.js"
+        }
+    }
+];
+
+module.exports = config;
--- index.js
+++ index.js
@@ -2,20 +2,40 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+const exec = require('child_process').exec;
+const config = require('./config/remote.config');
+const { DateTime } = require('luxon');
+
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+
+  app.log.info(getDateTime() + ' -> Mydogprobot loaded');
 
   app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return console.log('Issue opened!');
+    // TODO issues
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  // PUSH event
+  app.on("push", async (context) => {
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    const payload = context.payload;
+    const branch = payload.ref;
+    const repo = payload.repository.name;
+
+    // check config and do actions against existing entries
+    config.forEach(site => {
+      if(site.REPO === repo && branch === 'refs/heads/' + site.SETTINGS.BRANCH) {
+
+        exec('cd ' + site.SETTINGS.PATH + ' && ' + site.SETTINGS.EXEC);
+        app.log.info(getDateTime() + ' -> PUSH: ' + repo + ' [' + branch + '] pushed to remote');
+      }
+    });
+
+
+  });
+
 };
+
+// get current datetime
+function getDateTime() {
+  return DateTime.now().toUTC().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}
